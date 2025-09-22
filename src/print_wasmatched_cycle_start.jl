3980dd25c7fa3119ad10d568ad51a24fe76c4633
mtsch <matijacufar@gmail.com>
2025-09-22 21:41:01 +1200

    organize print_wasmatched_start

--- src/print_wasmatched_cycle_start.jl
+++ src/print_wasmatched_cycle_start.jl
@@ -1,6 +1,5 @@
 ## This code finds the cycle in year 1990 that we want to follow until 2020
 ### !!! When switching between julia and python be aware of index shift !!!
-
 using Ripserer
 using CSV
 
@@ -9,71 +8,81 @@ include("data-loading.jl")
 include("merge-trees.jl")
 include("plotting-utils.jl")
 
-data = load_data(1990)
-diagram = first(ripserer(Cubical(-data; threshold=-0.05); merge_tree=true, dim_max=0, reps=true))
+function select_first_child_of_largest()
+    data = load_data(1990)
+    diagram = ripserer(
+        Cubical(-data; threshold=-0.05);
+        merge_tree=true,
+        dim_max=0,
+        reps=true,
+    )[1]
 
-## Sort by area, descending to make it easier to find the largest one
-sort!(diagram; by=area, rev=true)
+    ## Sort by area, descending to make it easier to find the largest one
+    sort!(diagram; by=area, rev=true)
 
-## The diagram is a bit too big, so you should probably filter the tree. Below is the code
-## that keeps the intervals that have persistence larger than 0.05.
-filtered = filter_merge_tree(x -> persistence(x) ≥ 0.05, diagram)
+    ## The diagram is a bit too big, so you should probably filter the tree. Below is the
+    ## code that keeps the intervals that have persistence larger than 0.05.
+    filtered = filter_merge_tree(x -> persistence(x) ≥ 0.05, diagram)
 
-## If you want you can also drop all intervals that are not in a subtree with the largest one
-largest = filtered[1]
-filtered = merge_subtree(filtered, largest)
+    largest = filtered[1]
+    filtered = merge_subtree(filtered, largest)
 
-## Now you can look at the children to find the one that merged into it first or last
-## If we look at the min2, we get the child that is the first one to merge
-println("Min Child:", minimum(death, largest.children))
-println("Max Child:", maximum(death, largest.children))
-println("Min Child's Child:", minimum(death, largest.children[1].children))
-println("Max Child's Child:", maximum(death, largest.children[1].children))
-
-## We want to find the cycle with the corresponding death times
-
-
-selected = argmin(death, largest.children)
-
-diagram = first(ripserer(Cubical(-data; threshold=-0.05); merge_tree=true, dim_max=0, reps=true, verbose=true))
-
-selected_index = findfirst(diagram) do int
-    int.birth_simplex == selected.birth_simplex
+    selected = argmin(death, largest.children)
+    return selected
 end
 
-if isnothing(selected_index)
-    error("you messed up again!")
-end
-
-fig = Figure(size=(800, 600))
-
-plot_heatmap!(fig, data, title="1990")
-plot!(minimum_area_cycle(selected), color=:orangered3, markersize=2)
-
-save("wasmatch_1990.png", fig)
-
-@show selected_index
-for year in 1991:2020
-    matching = CSV.read("../data/matching_$(year-1)_$(year).csv", DataFrame)
-
-    matching_dict = Dict{Int,Int}(zip(matching[:,1] .+ 1, matching[:,2] .+ 1))
-
-    selected_index = matching_dict[selected_index]
-
-    diagram = first(
-        ripserer(
-            Cubical(-load_data(year); threshold=-0.05);
-            merge_tree=true, dim_max=0, reps=true, verbose=true,
-        )
-    )
-    selected = diagram[selected_index]
-
+function plot_a_cycle(data, interval; kwargs...)
     fig = Figure(size=(800, 600))
 
-    ax = plot_heatmap!(fig, data, title="$year")
-    plot!(minimum_area_cycle(selected), color=:orangered3, markersize=2)
+    ax = plot_heatmap!(fig, data; kwargs...)
+    plot!(ax, minimum_area_cycle(selected), color=:orangered3, markersize=2)
 
-    save("wasmatch_$(year).png", fig)
+    return fig
+end
 
-    @show selected_index
+function plot_matched_cycles(selected_interval)
+    diagram = ripserer(
+        Cubical(-data; threshold=-0.05);
+        # merge_tree=true,
+        dim_max=0,
+        reps=true,
+        verbose=true,
+    )[1]
+
+    selected_index = findfirst(diagram) do int
+        int.birth_simplex == selected_interval.birth_simplex
+    end
+    @info "Year 1990 selected index: $selected_index"
+
+    if isnothing(selected_index)
+        error("interval $selected_interval not found in persistence diagram!")
+    end
+
+    fig = plot_a_cycle(data, selected_interval; title=L"1990")
+    save("wasmatch_1990.png", fig)
+
+    for year in 1991:2020
+        matching = CSV.read("../data/matching_$(year-1)_$(year).csv", DataFrame)
+
+        matching_dict = Dict{Int,Int}(zip(matching[:,1] .+ 1, matching[:,2] .+ 1))
+
+        selected_index = matching_dict[selected_index]
+        @info "Year $year selected index: $selected_index"
+
+        diagram = first(
+            ripserer(
+                Cubical(-load_data(year); threshold=-0.05);
+                merge_tree=true, dim_max=0, reps=true, verbose=true,
+            )
+        )
+        selected = diagram[selected_index]
+
+        fig = plot_a_cycle(data, selected; title=L"%$year")
+        save("wasmatch_$(year).png", fig)
+    end
+end
+
+if !isinteractive()
+    selected_interval = select_first_child_of_largest()
+    plot_matched_cycles(selected_interval)
 end
